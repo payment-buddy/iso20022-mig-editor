8bb16a2908024cb09cfbbdeb8cef63346e661b77
Maksim Kammerer <268440869+payment-buddy@users.noreply.github.com>
2026-03-07 11:48:02 +0300

    Display element type name and improve the ElementDetail structure

--- src/ElementDetail.tsx
+++ src/ElementDetail.tsx
@@ -1,16 +1,27 @@
-import type {MessageElement} from "./types.ts";
+import type {DataType, MessageElement} from "./types.ts";
 
-export function ElementDetail({element}: { element: MessageElement }) {
+export function ElementDetail({element, dataType}: {
+    element: MessageElement,
+    dataType: DataType
+}) {
     return (
-        <dl>
-            <dt>Type</dt>
-            <dd>{element.typeId}</dd>
-            <dt>Multiplicity</dt>
-            <dd>[{element.minOccurs}..{element.maxOccurs}]</dd>
-            <dt>XML Tag</dt>
-            <dd>{element.xmlTag}</dd>
-            <dt>Description</dt>
-            <dd>{element.definition}</dd>
-        </dl>
+        <div>
+            <details open={true}>
+                <summary>Type</summary>
+                <div>{dataType.name}</div>
+            </details>
+            <details open={true}>
+                <summary>Description</summary>
+                <div>{element.definition}</div>
+            </details>
+            <details open={true}>
+                <summary>Multiplicity</summary>
+                <div>[{element.minOccurs}..{element.maxOccurs}]</div>
+            </details>
+            <details open={true}>
+                <summary>XML Tag</summary>
+                <div>{element.xmlTag}</div>
+            </details>
+        </div>
     )
 }
