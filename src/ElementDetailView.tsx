ebff93a6d6bb82f69d2802e28a1eaafa0039cfc6
Maksim Kammerer <268440869+payment-buddy@users.noreply.github.com>
2026-03-08 10:47:32 +0300

    Add Element Detail to MIG view

---
+++ src/ElementDetailView.tsx
@@ -0,0 +1,32 @@
+import type {DataType, MessageElement} from "./types.ts";
+
+export function ElementDetailView({element, dataType, xmlPath}: {
+    element: MessageElement
+    dataType: DataType
+    xmlPath: string[]
+}) {
+    return (
+        <div>
+            <details open={true}>
+                <summary>Type</summary>
+                <div>{dataType.name}</div>
+            </details>
+            <details open={true}>
+                <summary>Description</summary>
+                <div>{element.definition}</div>
+            </details>
+            <details open={true}>
+                <summary>Multiplicity</summary>
+                <div>[{element.minOccurs}..{element.maxOccurs}]</div>
+            </details>
+            <details open={true}>
+                <summary>XML Tag</summary>
+                <div>{element.xmlTag}</div>
+            </details>
+            <details open={true}>
+                <summary>XML PAth</summary>
+                <div>/{xmlPath.join("/")}</div>
+            </details>
+        </div>
+    )
+}
