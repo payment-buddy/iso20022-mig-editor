2bdc93a00780601371c824b662e31f2c4a99016c
Maksim Kammerer <268440869+payment-buddy@users.noreply.github.com>
2026-04-02 22:11:45 +0200

    Color code elements with overrides

--- src/components/ConstraintNode.tsx
+++ src/components/ConstraintNode.tsx
@@ -1,9 +1,10 @@
 import type {Constraint} from "../types/types.ts"
 import {useMessageTreeContext} from "../contexts/MessageTreeContext.tsx"
 
-export function ConstraintNode({constraint, parentPath}: {
+export function ConstraintNode({constraint, parentPath, isAdditional}: {
     constraint: Constraint
     parentPath: string
+    isAdditional?: boolean
 }) {
     const {selectedPath, onSelectConstraint} = useMessageTreeContext()
     const constraintPath = parentPath + '/' + constraint.name
@@ -12,7 +13,8 @@ export function ConstraintNode({constraint, parentPath}: {
         <div className={'tree-node' + (isSelected ? ' is-selected' : '')}
              style={{marginLeft: '1em'}}
              onClick={() => onSelectConstraint(constraint, constraintPath)}>
-            <span style={{fontSize: '0.8em'}}>✔</span> {constraint.name}
+            <span style={{fontSize: '0.8em'}}>✔ </span>
+            <span className={isAdditional ? ' has-override' : ''}>{constraint.name}</span>
         </div>
     )
 }
