7fbc747f27f4f5d86bc5f872b7c5a409b2f65f63
Maksim Kammerer <268440869+payment-buddy@users.noreply.github.com>
2026-05-03 08:19:22 +0200

    Gray-out child nodes of excluded element

--- src/components/ConstraintNode.tsx
+++ src/components/ConstraintNode.tsx
@@ -1,10 +1,11 @@
 import type {Constraint} from "../types/types.ts"
 import {useMessageTreeContext} from "../contexts/MessageTreeContext.tsx"
 
-export function ConstraintNode({constraint, parentPath, isAdditional}: {
+export function ConstraintNode({constraint, parentPath, isAdditional, parentExcluded}: {
     constraint: Constraint
     parentPath: string
     isAdditional?: boolean
+    parentExcluded?: boolean
 }) {
     const {selectedPath, filterActive, visiblePaths, onSelectConstraint} = useMessageTreeContext()
     const constraintPath = parentPath + '/' + constraint.name
@@ -14,7 +15,8 @@ export function ConstraintNode({constraint, parentPath, isAdditional}: {
         <div className={'tree-node' + (isSelected ? ' is-selected' : '')}
              onClick={() => onSelectConstraint(constraint, constraintPath)}>
             <span style={{fontSize: '0.8em'}}>✔</span>&nbsp;
-            <span className={isAdditional ? ' has-override' : ''}>{constraint.name}</span>
+            <span
+                className={(isAdditional ? ' has-override' : '') + (parentExcluded ? ' is-excluded' : '')}>{constraint.name}</span>
         </div>
     )
 }
